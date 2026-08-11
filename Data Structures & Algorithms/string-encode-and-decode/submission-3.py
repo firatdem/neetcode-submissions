dc66bc9ccf7464b1ab40be6d30492e25f366f1f3
firatdem <145790880+firatdem@users.noreply.github.com>
2026-08-11 15:26:26 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ""
+        for word in strs:
+            encoded_string += word + "`" # add word with space in front
+        print(encoded_string)
+        return encoded_string
+
+    def decode(self, s: strs) -> List[str]:
+        decoded_strs = []
+        word = ""
+        for char in s:
+            if char == "`":
+                decoded_strs.append(word)
+                word = ""
+            else:
+                word += char
+                #print(word)
+        return decoded_strs
